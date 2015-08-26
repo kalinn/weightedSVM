8050e00e12bcbf7be329a7f9a38e026be9d7226f
Kristin Linn <klinn@upenn.edu>
2015-08-26 09:12:16 -0400

    Cleaning up

---
+++ inst/fit_svm.py
@@ -0,0 +1,50 @@
+import numpy as np
+from sklearn import svm, grid_search
+
+def fit_weighted_svm(x, y, ts, C, weights):
+	#convert to numpy arrays
+	nX = np.array(x, ndmin=2)
+	ntsX = np.array(ts, ndmin=2)
+	nY = np.array(y, ndmin=1)/1.0
+	#for tuning
+	paramGrid = [
+	{'C': C, 'kernel': ['linear']}
+	]
+	#train
+	trnMdl = svm.SVC()
+	trnMdl_grid = grid_search.GridSearchCV(trnMdl, paramGrid, fit_params={'sample_weight': weights})
+	trnMdl_grid.fit(X=nX, y=nY)
+	trnMdl_best = trnMdl_grid.best_estimator_
+	#return SVM weights and predictions
+	trnMdl_rho = trnMdl_best.intercept_[0:1]
+	trnMdl_rho = list(trnMdl_rho)
+	trnMdl_w = trnMdl_best.coef_[0,:]
+	trnMdl_w = list(trnMdl_w)
+	yhats = trnMdl_best.predict(ntsX)
+	yhats = list(yhats)
+	bestC = trnMdl_grid.best_params_
+	return trnMdl_w, trnMdl_rho, yhats, bestC
+
+def fit_svm(x, y, ts, C):
+	#convert to numpy arrays
+	nX = np.array(x, ndmin=2)
+	ntsX = np.array(ts, ndmin=2)
+	nY = np.array(y, ndmin=1)/1.0
+	#for tuning
+	paramGrid = [
+	{'C': C, 'kernel': ['linear']}
+	]
+	#train
+	trnMdl = svm.SVC()
+	trnMdl_grid = grid_search.GridSearchCV(trnMdl, paramGrid)
+	trnMdl_grid.fit(X=nX, y=nY)
+	trnMdl_best = trnMdl_grid.best_estimator_
+	#return SVM weights and predictions
+	trnMdl_rho = trnMdl_best.intercept_[0:1]
+	trnMdl_rho = list(trnMdl_rho)
+	trnMdl_w = trnMdl_best.coef_[0,:]
+	trnMdl_w = list(trnMdl_w)
+	yhats = trnMdl_best.predict(ntsX)
+	yhats = list(yhats)
+	bestC = trnMdl_grid.best_params_
+	return trnMdl_w, trnMdl_rho, yhats, bestC
